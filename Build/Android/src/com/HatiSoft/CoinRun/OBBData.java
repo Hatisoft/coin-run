7820f2f73a23328cd406ce7607a01c7153e6f161
Kanekitoc <alvarojosepl@gmail.com>
2015-08-30 21:04:49 +0100

    added time score and time achievements to game

--- Build/Android/src/com/HatiSoft/CoinRun/OBBData.java
+++ Build/Android/src/com/HatiSoft/CoinRun/OBBData.java
@@ -16,8 +16,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"4", // the version of the APK that the file was uploaded against
-15224245 // the length of the file in bytes
+"5", // the version of the APK that the file was uploaded against
+15073378 // the length of the file in bytes
 )
 };
 };
